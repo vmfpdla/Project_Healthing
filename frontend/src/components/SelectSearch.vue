66e4e94468ef461865069b43d689f16c557b72af
vmfpdla <vmfpdla0@naver.com>
2020-03-06 21:24:59 +0900

    03/06 ADD Facility search funtion

--- frontend/src/components/SelectSearch.vue
+++ frontend/src/components/SelectSearch.vue
@@ -12,26 +12,32 @@
       </v-col>
       <v-col cols="12" sm="2">
         <v-overflow-btn
+          v-model="add2"
           class="my-2"
           :items="dropdown_add2"
           item-text="address2_name"
           label="지역선택2"
+          @change="add2_fun(add2)"
         ></v-overflow-btn>
       </v-col>
       <v-col cols="12" sm="2">
         <v-overflow-btn
+          v-model="fac"
           class="my-2"
           :items="dropdown_fac"
           item-text="address_name"
           label="시설선택"
+          @change="fac_fun(fac)"
         ></v-overflow-btn>
       </v-col>
 
       <v-col cols="12" sm="2">
         <v-overflow-btn
+          v-model="cate"
           class="my-2"
           :items="dropdown_category"
           label="카테고리"
+          @change="cate_fun(cate)"
         ></v-overflow-btn>
       </v-col>
 
@@ -42,7 +48,7 @@
         ></v-text-field>
       </v-col>
       <v-col cols="12" sm="2">
-        <v-btn class="ma-2" outlined color="indigo" @click=select()>Search</v-btn>
+        <v-btn class="ma-2" outlined color="indigo" @click="select_search">Search</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -52,50 +58,117 @@
 import axios from 'axios'
   export default {
     data: () => ({
-      add1 : "",
+      select_data:{
+        add1 : "",
+        add2 : "",
+        fac : "",
+        cate : "",
+      },
       dropdown_add: [
         {text:"서울시"},
         {text:"경기도"},
       ],
       dropdown_add2: [],
       dropdown_fac: [
-        { id:'a',"address_name" :'abc', "textt" : 'list'},
-        { id:'b',"address_name" :'abcd', "textt" : 'favorite'},
-        { id:'c',"address_name" :'abce', "textt" : 'delete'},
+        "공공",
+        //{ text: "사설"},
       ],
       dropdown_category: [
-        
+        "스쿼시",
+        "수영",
+        "필라테스",
+        "인라인",
+        "줄넘기",
+        "유아체능단(분기별)",
+        "댄스",
+        "탁구",
+        "헬스",
+        "요가",
+        "아쿠아로빅",
+        "에어로빅",
+        "축구",
+        "무용",
+        "유아체육(월단위)",
+        "농구",
+        "검도",
+        "발레",
+        "장애인체육",
+        "골프",
+        "태보",
+        "무술",
+        "배드민턴",
+        "암벽등반",
+        "합기도",
+        "체조",
+        "태권도",
+        "단전",
+        "풋살",
+        "스피드스케이트",
+        "테니스",
+        "보드(S-BOARD)",
+        "게이트볼",
+        "궁도",
+        "당구포켓볼",
+        "권투",
+        "학교체육",
+        "야구",
+        "유도",
+        "배구",
+        "스키",
+        "조깅/워킹",
+        "피겨스케이팅",
+        "라켓볼",
+        "하키",
+        "사이클(자전거)",
+        "승마"
       ],
       
     }),
     methods : {
       add_fun(add1){
         this.dropdown_add2=[];
+        console.log(this.dropdown_add2);
         axios.get('/api/select_add')
           .then((response) => {
             if(add1=="서울시"){
+              this.select_data.add1=add1;
               for(var i=0;i<response.data.length;i++)
               {
                 if(response.data[i].address1_name==add1)
                 {
                   this.dropdown_add2.push(response.data[i].address2_name)
-                  console.log(this.dropdown_add2);
+                  
                 }
               }
             }
             else{
-                for(var j=0;i<response.data.length;j++)
+              this.select_data.add1=add1;
+              for(var j=0;i<response.data.length;j++)
+              {
+                if(response.data[j].address1_name==add1)
                 {
-                  if(response.data[j].address1_name==add1)
-                  {
-                    this.dropdown_add2.push(response.data[j].address2_namee)
-                    console.log(this.dropdown_add2);
-                  }
+                  this.dropdown_add2.push(response.data[j].address2_name)
+                  
                 }
-            }
-          })
+              }
+          }
+        })
+      },
+      add2_fun(add2){
+        this.select_data.add2= add2;
+      },
+      fac_fun(fac){
+        console.log(fac);
+        this.select_data.fac = fac;
+      },
+      cate_fun(cate){
+        this.select_data.cate = cate;
+      },
+      select_search(){
+        console.log("눌렀다");
+        this.$emit("select-event",this.select_data);
       }
-    }
+    },
     // created () { // 컴포넌트가 생성될 때, 요청을 보낸다.        
     //   axios.get('/api/select_add')
     //     .then((response) => {
--- frontend/src/pages/Facility.vue
+++ frontend/src/pages/Facility.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container class="ma-5 pa-5">
-        <SelectSearch/>
+        <SelectSearch @select-event="parents_select"/>
         <Sort/>
         <v-row 
             v-for="n in 10"
@@ -12,7 +12,10 @@
                 v-for="k in 4"
                 :key="k"
             >   
-                <FacilityCard v-if="loading==true" v-bind:senddata="CENTERS[(n-1)*4+k-1]"/>     
+                <FacilityCard 
+                    v-if="loading==true" 
+                    v-bind:senddata="CENTERS[(n-1)*4+k-1]"
+                />     
             </v-col>
         </v-row>  
     </v-container>
@@ -30,15 +33,39 @@ import axios from 'axios';
             return {
                 CENTERS:[],
                 loading:false,
+                select_data:[],
+
             }
         },
         created(){
-            axios.get('http://openAPI.seoul.go.kr:8088/4c70455157766d663130356e6e615668/json/ListProgramByPublicSportsFacilitiesService/1/10000/')
+            axios.get('http://openAPI.seoul.go.kr:8088/4c70455157766d663130356e6e615668/json/ListProgramByPublicSportsFacilitiesService/1/1000/')
                 .then((result)=>{
                     this.CENTERS = result.data.ListProgramByPublicSportsFacilitiesService.row
                     this.loading = true
-                    console.log(this.CENTRS)
                 })
+        },
+        methods:{
+            parents_select(message){
+                this.loading= false;
+                this.select_data=message;
+                this.CENTERS=[];
+                axios.get('http://openAPI.seoul.go.kr:8088/4c70455157766d663130356e6e615668/json/ListProgramByPublicSportsFacilitiesService/1/1000/'+message.cate)
+                // 카테고리를 url 로 고른다음에
+                .then((result)=>{
+                    //var temp =[];
+                    if(this.select_data.add1=="서울시"){ // 만약 서울시를 선택하면
+                        for(var i=0;i<result.data.ListProgramByPublicSportsFacilitiesService.row.length;i++)
+                        {
+                            if(result.data.ListProgramByPublicSportsFacilitiesService.row[i].ADDRESS.includes(this.select_data.add2)==true){  // 종로구를 선택시 종로구만 나오게
+                                this.CENTERS.push(result.data.ListProgramByPublicSportsFacilitiesService.row[i]);
+                            }
+                        }
+                        this.loading = true;
+                        //this.CENTERS = temp;
+                    }
+                })
+            },
+ 
         }
     }
  
